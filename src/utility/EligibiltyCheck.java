0875e20c1fc2a590ed3f9425d72dc48e61597f17
unknown <shrikant2994anil@gmail.com>
2020-03-27 20:13:19 +0530

    FSJ0320A005

--- src/utility/EligibiltyCheck.java
+++ src/utility/EligibiltyCheck.java
@@ -1,5 +1,57 @@
 package utility;
 
+import model.User;
+
+//Progression - 4
+
+public class EligibilityCheck extends BasicEligibility implements EligibilityInterface{
+	
+	public boolean basicEligibilityCheck(User user) {
+		if(checkUser(user)&&checkQuizAnswer("points"))
+			return true;
+		else
+		   return false;
+	
+	}
+	
+	// Progression - 5
+	public boolean checkUser(User user) {
+		 int age=user.getAge();
+		
+		 int height=user.getHeight();
+		 int weight=user.getWeight();
+	     String country=user.getCountry();
+		
+		if((18<=age && age<=35)&&(155 <= height && height<=170)&&(55<=weight&&weight<=90)&&(country.equals("ProGrad")))
+		return true;
+		else
+		return false;
+	}
+
+	
+	
+ // Progression - 6
+	@Override
+	public boolean checkQuizAnswer(String points) {
+		int num = Integer.parseInt(points);
+		
+		if(num>80) 
+			return true;
+		else
+		
+		   return false;
+	}
+
+
+	
+}
+
+
+
+
+
+
+
 
 
 
